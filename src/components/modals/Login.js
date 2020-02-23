f5c3d1a764fc7de6d76a08d5f9eb173ff4646833
owaaquino <josejoshuaaquino@gmail.com>
2020-02-23 16:58:39 +0800

    Add error catching

--- src/components/modals/Login.js
+++ src/components/modals/Login.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../../firebase";
 
 function Login(props) {
+  const [isError, setError] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     let userName = e.target.elements.userEmail.value;
     let password = e.target.elements.password.value;
 
-    auth.signInWithEmailAndPassword(userName, password).then(cred => {
-      props.closeModal();
-    });
+    auth
+      .signInWithEmailAndPassword(userName, password)
+      .then(cred => {
+        props.closeModal();
+      })
+      .catch(err => {
+        setError(err.message);
+      });
   }
 
   return (
@@ -22,6 +28,7 @@ function Login(props) {
         <label htmlFor="password">Password</label>
         <input type="password" name="password" />
       </p>
+      <p style={{ color: `tomato` }}>{isError}</p>
       <button>Login</button>
     </form>
   );
